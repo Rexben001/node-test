e7986becf4c3589e85828e9f650b3e2508e688c6
Rexben <rexben.rb@gmail.com>
2021-11-25 21:43:23 +0100

    update port number

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const { User, connectWithRetry } = require('./models');
-const PORT = process.env.PORT || 4100;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
